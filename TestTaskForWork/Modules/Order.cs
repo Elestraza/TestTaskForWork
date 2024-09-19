5258f6cb32f39c6b6a0661c84b6d8b371c573dfb
EGAProg <gameswithiy@mail.ru>
2024-09-19 12:45:53 +0300

    Test version. More SQL procedures, edited interface code for it

--- TestTaskForWork/Modules/Order.cs
+++ TestTaskForWork/Modules/Order.cs
@@ -4,6 +4,7 @@
     {
         public int id { get; set; }
         public DateTime orderdate { get; set; }
+        public DateTime duedate { get; set; }
         public bool iscancelled { get; set; }
 
         public ICollection<ordernomenclature> ordernomenclatures { get; set; }
